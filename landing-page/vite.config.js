496754a0978c6843de4b14f1ba758af6acf5ba81
Malcolm Franklin <mac.mac5@yahoo.com>
2024-11-28 11:15:34 -0700

    updated path to main jsx

--- landing-page/vite.config.js
+++ landing-page/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Ensures relative paths in production
   plugins: [react()],
-  server: {
-    open: true, // this will open the browser automatically when you run the dev server
-  },
   build: {
-    outDir: 'dist', // Output directory for built files
+    outDir: 'dist', // Specifies the output folder for built files
+    rollupOptions: {
+      input: './index.html', // Ensures Rollup starts from the correct entry point
+    },
   },
 });
